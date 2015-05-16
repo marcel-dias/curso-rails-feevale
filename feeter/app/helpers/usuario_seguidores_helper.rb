391f38c0562a6327486af174a1eee49697f97540
Marcel Dias <marceldiass@gmail.com>
2015-05-16 12:41:12 -0300

    add usuario seguidores selection to helpers

--- feeter/app/helpers/usuario_seguidores_helper.rb
+++ feeter/app/helpers/usuario_seguidores_helper.rb
@@ -1,2 +1,9 @@
 module UsuarioSeguidoresHelper
+  def usuario_select(campo)
+    collection_select :usuario_seguidor,
+              campo,
+              Usuario.all,
+              :id,
+              :nome
+  end
 end
