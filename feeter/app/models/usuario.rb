31a108d95b21994b209164d67c9ecc33b22a44a9
Marcel Dias <marceldiass@gmail.com>
2015-05-16 12:41:59 -0300

    add timeline relationship

--- feeter/app/models/usuario.rb
+++ feeter/app/models/usuario.rb
@@ -1,2 +1,15 @@
 class Usuario < ActiveRecord::Base
+  has_many :segue,
+            :class_name => "UsuarioSeguidor",
+            :foreign_key => :seguidor_id
+
+  has_many :usuarios_seguidos,
+            :through => :segue,
+            :source => :seguido
+
+  has_many :timeline,
+            :through => :usuarios_seguidos,
+            :source => :tweets
+
+  has_many :tweets
 end
